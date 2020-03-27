89ab8bd68ea0eaca3c881f065387045ff6c6b684
lixiang <lixiangers@gmail.com>
2020-03-27 22:39:31 +0800

    完善二分变种

--- binarysearch/binarysearch.go
+++ binarysearch/binarysearch.go
@@ -96,3 +96,97 @@ func BinarySearchForLast(array []int, target int) int {
 	}
 	return result
 }
+
+// 寻找第一个大于等于给定的值
+func BinarySearchForFirstGreaterThanOrEqual(array []int, target int) int {
+	result := -1
+	if nil == array {
+		return result
+	}
+
+	lowIndex := 0
+	highIndex := len(array) - 1
+
+	for lowIndex <= highIndex {
+		middle := lowIndex + (highIndex-lowIndex)>>1
+
+		if array[middle] >= target {
+			// 发现一个大于等于target的值，需要判断是否已经是第一个符合的条件。
+			// 如果已经是数组额左边边，或者middle-1小于target，就说明是的第一个符合的条件
+			if middle == 0 || array[middle-1] < target {
+				result = middle
+				break
+			} else {
+				// 说明满足条件的值还在middle左边
+				highIndex = middle - 1
+			}
+		} else {
+			lowIndex = middle + 1
+		}
+	}
+
+	return result
+}
+
+// 查找最后一个小于等于给定target的值
+func BinarySearchForLastLessThanOrEqual(array []int, target int) int {
+	result := -1
+	if nil == array {
+		return result
+	}
+
+	lowIndex := 0
+	highIndex := len(array) - 1
+
+	for lowIndex <= highIndex {
+		middle := lowIndex + (highIndex-lowIndex)>>1
+
+		if array[middle] <= target {
+			// 发现一个小于等于target的值，需要判断是否已经是最后个符合的条件。
+			// 如果已经是数组最右边，或者middle-1大于target，就说明是的最后一个符合的条件
+			if middle == len(array)-1 || array[middle+1] > target {
+				result = middle
+				break
+			} else {
+				// 说明满足条件的值还在middle右边
+				lowIndex = middle + 1
+			}
+		} else {
+			highIndex = middle - 1
+		}
+	}
+
+	return result
+}
+
+// 对于一个V行数组，找出最低端的值 [6,5,4,3,4,7,8,10]  3
+// 左边大于它，右边也大于它的值.
+func BinarySearchFindSplitPoint(array []int) int {
+	result := -1
+	if nil == array {
+		return result
+	}
+
+	lowIndex := 0
+	highIndex := len(array) - 1
+
+	for lowIndex <= highIndex {
+		middle := lowIndex + (highIndex-lowIndex)>>1
+		if middle == 0 || middle == len(array)-1 {
+			//当已经是左边或最右边的表示没有这样的值
+			break
+		} else if array[middle-1] > array[middle] && array[middle+1] > array[middle] {
+			// 说明找到做了这个值
+			result = middle
+			break
+		} else if array[middle+1] > array[middle] {
+			// 说明是在右边的递增区域,想要的值在middle的左边
+			highIndex = middle - 1
+		} else {
+			// 说明是在左边的递减区域,想要的值在middle的右边
+			lowIndex = middle + 1
+		}
+	}
+
+	return result
+}
--- binarysearch/binarysearch_test.go
+++ binarysearch/binarysearch_test.go
@@ -10,8 +10,20 @@ func TestBinarySearch(t *testing.T) {
 
 	fmt.Println(StandardBinarySearch(array, 12))
 
-	array1 := []int{1, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4}
+	array1 := []int{1, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 6, 6, 7}
 	fmt.Println(BinarySearchForFirst(array1, 4))
 	fmt.Println(BinarySearchForLast(array1, 4))
+	fmt.Println(BinarySearchForFirstGreaterThanOrEqual(array1, 5))
+	fmt.Println(BinarySearchForFirstGreaterThanOrEqual(array1, 3))
+	fmt.Println(BinarySearchForLastLessThanOrEqual(array1, 6))
+	fmt.Println(BinarySearchForFirst(array1, 4))
+
+	array2 := []int{6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 7, 8}
+	fmt.Println(BinarySearchFindSplitPoint(array1))
+	fmt.Println(BinarySearchFindSplitPoint(array2))
+
+}
+
+func ExampleBinarySearchForFirstGreaterThanOrEqual() {
 
 }
