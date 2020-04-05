a0f1d6f1a47f81a7b0a3dc011dca9dd4c004520d
lixiang <lixiangers@gmail.com>
2020-04-05 17:15:54 +0800

    动态规划0/1背包升级版

--- highlevel/zerooronebagdp.go
+++ highlevel/zerooronebagdp.go
@@ -41,3 +41,63 @@ func KnapsackDP(items []int, n int, maxWeight int) int {
 
 	return 0
 }
+
+// 在0/1背包问题的前提下，我们引入物品对于的价值value.求在不超过背包重量的前提下，价值最大是多少
+// 把整个求解过程分为 n 个阶段，每个阶段会决策一个物品是否放到背包中。每个阶段决策完之后，背包中的物品的总重量以及总价值.
+// 我们使用state[n][weight+1]来保存每层的不同状态．不过这里我们存储的是对于的最大总价值.
+// 每层放入的情况还是分weight+1种，但是它对应的值是最大的值.
+//@items 物品各自的重量
+//@values 物品各自的价值
+//@items 物品的个数
+//@maxWeight 背包支持的最大重量
+func KnapsackDP2(items []int, values []int, n int, maxWeight int) int {
+	states := make([][]int, n)
+
+	// 初始化状态
+	for i := 0; i < n; i++ {
+		intslice := make([]int, maxWeight+1)
+		for j := 0; j < maxWeight+1; j++ {
+			intslice[j] = -1
+		}
+
+		states[i] = intslice
+	}
+
+	// 初始化第一个物品
+	states[0][0] = 0 //第一个物品不放入
+	if items[0] <= maxWeight {
+		//第一个物品放入
+		states[0][items[0]] = values[0]
+	}
+
+	// 依次推导每层的状态
+	for i := 1; i < n; i++ {
+		for j := 0; j < maxWeight+1; j++ {
+			// 不放入物品
+			if states[i-1][j] >= 0 {
+				states[i][j] = states[i-1][j]
+			}
+		}
+
+		for j := 0; j < maxWeight+1; j++ {
+			// 放入物品.需要判断不要超过maxWeight
+			if states[i-1][j] >= 0 && j+items[i] <= maxWeight {
+				currValue := states[i-1][j] + values[i]
+				if currValue > states[i][j+items[i]] {
+					// 保存最大值
+					states[i][j+items[i]] = currValue
+				}
+			}
+		}
+	}
+
+	//找到最大值
+	maxValue := 0
+	for i := 0; i <= maxWeight; i++ {
+		if states[n-1][i] > maxValue {
+			maxValue = states[n-1][i]
+		}
+	}
+
+	return maxValue
+}
--- highlevel/zerooronebagdp_test.go
+++ highlevel/zerooronebagdp_test.go
@@ -8,3 +8,7 @@ import (
 func TestKnapsackDP(t *testing.T) {
 	fmt.Println(KnapsackDP([]int{2, 2, 4, 6, 3}, 5, 9))
 }
+
+func TestKnapsackDP2(t *testing.T) {
+	fmt.Println(KnapsackDP2([]int{2, 2, 4, 6, 3}, []int{3, 4, 8, 9, 6}, 5, 9))
+}
